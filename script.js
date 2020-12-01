672ccffe9c6e634b1ea2bc743546c1921a426c41
Arthur Camurça <arthur.04.pb@gmail.com>
2020-12-01 10:33:48 -0300

    adicionando menu fixo e menu ancora

---
+++ script.js
@@ -0,0 +1,30 @@
+//identificar o clique no menu
+//verificar o item que foi clicado e fazer referência com o alvo
+//verificar a distância entre o alvo e o topo
+//animar o scroll até o alvo
+
+const menuItems = document.querySelectorAll('.menu a[href^="#"]');
+
+menuItems.forEach(item => {
+  item.addEventListener('click', scrollToIdOnClick);
+})
+
+function scrollToIdOnClick(event){
+  event.preventDefault();
+  const $linkMenu = event.target;
+  const to = getScrollTopByHref($linkMenu);
+  scrollToPosition(to);
+}
+
+function scrollToPosition(position){
+  window.scroll({
+    top: position,
+    behavior: "smooth",
+  });
+}
+
+function getScrollTopByHref(element){
+  const id = element.getAttribute('href');
+  const $section = document.querySelector(id);
+  return $section.offsetTop;
+}
